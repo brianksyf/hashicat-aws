7645d47ff709b8d0696f2a70c312ca9f6565cc39
brianksyf <110999193+brianksyf@users.noreply.github.com>
2022-08-10 14:23:00 -0400

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "terraform-aws-modules/terraform-aws-s3-bucket"
 
   bucket = "testetetetmy-s3-bucket"
   acl    = "private"
