6bfe55f21447499438d2a8df1eb43fae451ea397
BrianK <brian.klobas@syf.com>
2022-08-10 19:19:17 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
 module "s3_bucket" {
   source  = "app.terraform.io/SynchronyFinancial/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "BrianKlobasmy-s3-bucket"
-  acl    = "private"
+  bucket_prefix = "BrianKlobas"
 }
